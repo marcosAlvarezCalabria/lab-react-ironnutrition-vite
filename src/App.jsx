5887e00d3392edad44fbf65b6d01af008207eb8d
marcosAlvarezCalabria <marcosalvarezcalabria@gmail.com>
2024-04-08 11:10:57 +0100

    Feat: iteration 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import foodsJson from "./foods.json";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [foods, setFoods] = useState(foodsJson);
+  
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
